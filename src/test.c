da14c15bd10e1e36426e90d97efe5646821d1914
Nick Kasten <kastencode@gmail.com>
2016-12-12 13:07:05 -0600

    added changes

--- src/test.c
+++ src/test.c
@@ -7,6 +7,9 @@
 
 #define BUZZ_PIN		1
 
+#define TRIG_PIN		2	
+#define ECHO_PIN		4
+
 #define RED_ON          (PORTB |= (0x0F))
 #define RED_OFF         (PORTB &= ~(1<<RED_PIN))
 #define GREEN_ON        (PORTB |= (0x10))
@@ -14,53 +17,49 @@
 #define YELLOW_ON		(PORTB |= (1<<RED_PIN) | (1<<GREEN_PIN))
 #define YELLOW_OFF		(PORTB &= (0x00))
 
+#define TRIG_ON			(PORTD |= (1<<TRIG_PIN))
+#define	TRIG_OFF		(PORTD &= ~(1<<TRIG_PIN))
+
+#define SONAR_CONFIG	(DDRD |= (1<<TRIG_PIN))
 #define OUTPUT_CONFIG   (DDRB |= (1<<RED_PIN) | (1<<GREEN_PIN) | (1<<BUZZ_PIN))
 #define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
 
-int ticks = 0;
-int state = 1;
+// Sonar 
+int getDistance() {
+  // establish variables for duration of the ping, 
+  // and the distance result in centimeters:
+  long duration, centimeters;
 
-void buzzToggle(void) {
-	if (state == 1) {
-		PORTB &= ~(1<<BUZZ_PIN);
-		state = 0;
-	} else {
-		PORTB |= (1<<BUZZ_PIN);
-		state = 1;
-	}
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+	TRIG_OFF;
+	_delay_ms(2);
+	TRIG_ON;
+	_delay_ms(10);
+	TRIG_OFF;
 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(ECHO_PIN, INPUT);
+  duration = pulseIn(ECHO_PIN, HIGH, 20);
+
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  centimeters = duration / 29 / 2;
+
+    return centimeters;
 }
 
-void buzzEnable(void) {
-	for (;;) {
-		ticks++;
-		if (ticks == 60) {
-			ticks = 0;
-			buzzToggle();
-		}
-	}	
-}
 
 int main(void) {
 
     CPU_PRESCALE(2);
-    OUTPUT_CONFIG;
-    RED_OFF;
-	GREEN_OFF;
-
-    while(1){
-        RED_ON;
-        _delay_ms(500);
-		RED_OFF;
-        _delay_ms(500);
-		GREEN_ON;
-		buzzEnable();
-        _delay_ms(500);
-		GREEN_OFF;
-        _delay_ms(500);
-		YELLOW_ON;
-        _delay_ms(500);
-		YELLOW_OFF;
-        _delay_ms(500);
-    }
+    SONAR_CONFIG;
+	OUTPUT_CONFIG;
+    
+	while(1){
+    
+	}
 }
